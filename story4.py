89ef5e9cf8b02c817c84f46eb87fa7f35705bd91
Lai05927 <laiyanmancsptt10@gmail.com>
2024-05-12 22:58:50 +0800

    add background for story 4

---
+++ story4.py
@@ -0,0 +1,10 @@
+import tkinter 
+from tkinter import PhotoImage 
+root = tkinter.Tk()
+root.title('Level 4')
+root.geometry("1280x720")
+image_path=PhotoImage(file=r"C:\CSP1123\Project\background storyline 4(3).png")
+bg_image=tkinter.Label(root,image=image_path )
+bg_image.pack()
+
+root.mainloop()
