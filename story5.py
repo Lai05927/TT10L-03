61f3fde433dcdc0a3d1f3a71a34ec1ba5e16f11a
Lai05927 <laiyanmancsptt10@gmail.com>
2024-05-12 21:18:40 +0800

    adding background

--- story5.py
+++ story5.py
@@ -6,4 +6,5 @@ root.geometry("1280x720")
 image_path=PhotoImage(file=r"C:\CSP1123\Project\background storyline 5.png")
 bg_image=tkinter.Label(root,image=image_path )
 bg_image.pack()
+
 root.mainloop()
