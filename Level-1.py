5d8e7962ba43a615045292f30a2d6ca70dac1207
Yuner25 <thooyuner25@gmail.com>
2024-05-15 20:20:32 +0800

    png image transparency fixed

--- Level-1.py
+++ Level-1.py
@@ -10,28 +10,24 @@ canvas = tk.Canvas(root, width=1280, height=720)
 canvas.pack()
 canvas.create_image(0, 0, anchor="nw", image=bg_photo)
 
-def open_hall_window():
+def open_hall_window(event):
     hall_window = tk.Toplevel(root)
     hall_window.title("Hall")
     hall_window.geometry(root.geometry())
     tk.Label(hall_window, text="This is a new window!").pack()
 
-hall_button_image = Image.open("Image/Level_1/Hall_Button.png")
-hall_button_photo = ImageTk.PhotoImage(hall_button_image)
-hall_button = tk.Button(root, image=hall_button_photo, command=open_hall_window)
-hall_button.photo = hall_button_photo
-hall_button.place(x=540, y=150)
+hall_button_image = ImageTk.PhotoImage(Image.open("Image\Level_1\Hall_Button.png"))
+hall_button = canvas.create_image(687, 240, image=hall_button_image)
+canvas.tag_bind(hall_button, "<Button-1>", open_hall_window)
 
-def open_room_window():
+def open_room_window(event):
     room_window = tk.Toplevel(root)
     room_window.title("Room")
     room_window.geometry(root.geometry())
     tk.Label(room_window, text="This is a new window!").pack()
 
-room_button_image = Image.open("Image/Level_1/Room_Button.png")
-room_button_photo = ImageTk.PhotoImage(room_button_image)
-room_button = tk.Button(root, image=room_button_photo, command=open_room_window)
-room_button.photo = room_button_photo
-room_button.place(x=20, y=200)
+room_button_image = ImageTk.PhotoImage(Image.open("Image\Level_1\Room_Button.png"))
+room_button = canvas.create_image(243, 390, image=room_button_image)
+canvas.tag_bind(room_button, "<Button-1>", open_room_window)
 
 root.mainloop()
