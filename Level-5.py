9b7511665bd24f62d13d3fc47d31c621941f16ce
Yuner25 <thooyuner25@gmail.com>
2024-05-15 22:11:40 +0800

    File reorganization due to level rearrange

--- Level-5.py
+++ Level-5.py
@@ -4,7 +4,7 @@ root = tk.Tk()
 root.title("Level 1")
 root.geometry("1280x720+0+0")
 
-bg_image = Image.open("Image/Level_1/Background.png")
+bg_image = Image.open("Level_1/Image/Background.png")
 bg_photo = ImageTk.PhotoImage(bg_image)
 canvas = tk.Canvas(root, width=1280, height=720)
 canvas.pack()
@@ -16,7 +16,7 @@ def open_hall_window(event):
     hall_window.geometry(root.geometry())
     tk.Label(hall_window, text="This is a new window!").pack()
 
-hall_button_image = ImageTk.PhotoImage(Image.open("Image\Level_1\Hall_Button.png"))
+hall_button_image = ImageTk.PhotoImage(Image.open("Level_1/Image/Hall_Button.png"))
 hall_button = canvas.create_image(687, 240, image=hall_button_image)
 canvas.tag_bind(hall_button, "<Button-1>", open_hall_window)
 
@@ -26,7 +26,7 @@ def open_room_window(event):
     room_window.geometry(root.geometry())
     tk.Label(room_window, text="This is a new window!").pack()
 
-room_button_image = ImageTk.PhotoImage(Image.open("Image\Level_1\Room_Button.png"))
+room_button_image = ImageTk.PhotoImage(Image.open("Level_1/Image/Room_Button.png"))
 room_button = canvas.create_image(243, 390, image=room_button_image)
 canvas.tag_bind(room_button, "<Button-1>", open_room_window)
 
