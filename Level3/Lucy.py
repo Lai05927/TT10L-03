7eecd38664a40f4634cee44a706e948a406fabc8
Lai05927 <laiyanmancsptt10@gmail.com>
2024-05-20 00:55:34 +0800

    add a new window for lucy

---
+++ Level3/Lucy.py
@@ -0,0 +1,10 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+from tkinter import messagebox
+import subprocess
+
+root = tk.Tk()
+root.title('Level 4')
+root.geometry("1280x720")
+
+root.mainloop()
