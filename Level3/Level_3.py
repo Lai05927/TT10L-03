ae9f1c164d7a68eb47390ac913da2bcad57d631a
Lai05927 <laiyanmancsptt10@gmail.com>
2024-05-28 00:10:32 +0800

    add home and back button , and link it with index & lucy window

--- Level3/Level_3.py
+++ Level3/Level_3.py
@@ -2,23 +2,25 @@ from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter import messagebox
 import subprocess
-root = tk.Tk()
-root.title('Level 3')
-root.geometry("1280x720")
+Level_3 = tk.Tk()
+Level_3.title('Level 3')
+screen_width = Level_3.winfo_screenwidth()
+screen_height = Level_3.winfo_screenheight()
+Level_3.geometry(f"{screen_width}x{screen_height}")
 
 # Load background image
 image_story4 = ImageTk.PhotoImage(file=r"Level3\Image\storyline 4 background.png")
 bg_image =image_story4
 
 # Create canvas
-canvas = tk.Canvas(root, width=1280, height=720)
+canvas = tk.Canvas(Level_3, width=screen_width, height=screen_height)
 canvas.pack(fill='both',expand=True)
 
 # Display background image on canvas
-canvas.create_image(0, 0, anchor='nw', image=bg_image)
+canvas.create_image(screen_width/2, screen_height/2, anchor='center', image=bg_image)
 
 txt = '\n\n\n\n\nYesterday Lucy and her colleagues Peter,Jart and Ava went camping\ntogether. That night, everyone got drunk...'
-text_item = canvas.create_text(640,40, anchor='center', text=txt, fill='white', font=('Arial', 24, 'bold'))
+text_item = canvas.create_text(screen_width/2,50/864*screen_height, anchor='center', text=txt, fill='white', font=('Arial', 24, 'bold'))
 
 index = 0
 
@@ -31,7 +33,7 @@ def slider():
     else:
         canvas.itemconfig(text_item, text=txt[:index])
         index += 1
-        root.after(40, slider)
+        Level_3.after(40, slider)
 
 slider()
 
@@ -53,15 +55,33 @@ def show_message(event):
     messagebox.showinfo("Clue for Ava","1.She likes Peter")
 
 button_image= ImageTk.PhotoImage(Image.open("Level3\Image\Ava.png"))
-button=canvas.create_image(797,500, image=button_image)
+button=canvas.create_image(893/1536*screen_width,547/864*screen_height, image=button_image)
 canvas.tag_bind(button,"<Button-1>", show_message)
 
 def open_Lucy_window(event):
     subprocess.Popen(["python", "Level3/Lucy.py"])
-    root.destroy()
+    Level_3.destroy()
 
 Lucy_button_image = ImageTk.PhotoImage(Image.open("Level3\Image\Lucy.png"))
-lucy_button = canvas.create_image(890, 510, image=Lucy_button_image)
+lucy_button = canvas.create_image(987/1536*screen_width, 560/864*screen_height, image=Lucy_button_image)
 canvas.tag_bind(lucy_button, "<Button-1>", open_Lucy_window)
     
-root.mainloop()
+def home(event):
+    subprocess.Popen(["python","Index.py"])
+    Level_3.destroy()
+
+home_image = (Image.open("Level3\Image\Home.png")).resize((45,45))
+home_image_tk = ImageTk.PhotoImage(home_image)
+home_button = canvas.create_image(140/1536*screen_width, 80/864*screen_height, image=home_image_tk)
+canvas.tag_bind(home_button, "<Button-1>", home)
+
+def back(event):
+    subprocess.Popen(["python","Level3/Level_3.py"])
+    Level_3.destroy()
+
+back_image = (Image.open("Level3\Image\Back.png")).resize((45,45))
+back_image_tk = ImageTk.PhotoImage(back_image)
+back_button = canvas.create_image(200/1536*screen_width, 80/864*screen_height, image=back_image_tk)
+canvas.tag_bind(back_button, "<Button-1>", back)
+
+Level_3.mainloop()
