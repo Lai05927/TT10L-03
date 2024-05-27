ad4f7592c24ed787dda47bb16d9c4ddf1735211d
Yuner25 <thooyuner25@gmail.com>
2024-05-27 12:21:22 +0800

    add glow effect to button

--- Level_5.py
+++ Level_5.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageFilter, ImageEnhance, ImageOps
 import tkinter as tk
 import subprocess
 root = tk.Tk()
@@ -23,11 +23,27 @@ home_image_tk = ImageTk.PhotoImage(home_image)
 home_button = canvas.create_image(30/1280*screen_width, 30/720*screen_height, image=home_image_tk)
 canvas.tag_bind(home_button, "<Button-1>", home)
 
+def create_glow_effect(image, glow_color=(200, 200, 200, 255), radius=15):
+    image = image.convert("RGBA")
+    mask = image.split()[3]
+    glow = Image.new("RGBA", image.size, glow_color)
+    glow.putalpha(mask)
+    expanded_mask = mask.resize((mask.width + 2 * radius, mask.height + 2 * radius), Image.LANCZOS)
+    blurred_mask = expanded_mask.filter(ImageFilter.GaussianBlur(radius))
+    glow_image = Image.new("RGBA", (image.width + 2 * radius, image.height + 2 * radius), (0, 0, 0, 0))
+    expanded_glow = glow.resize((image.width + 2 * radius, image.height + 2 * radius), Image.LANCZOS)
+    glow_image.paste(expanded_glow, (0, 0), mask=blurred_mask)
+    glow_image.paste(image, (radius, radius), mask=image)
+    
+    return glow_image
+
 def open_hall_window(event):
     subprocess.Popen(["python", "Level_5/Hall.py"])
     root.destroy()
 
-hall_button_image = ImageTk.PhotoImage(Image.open("Level_5/Image/Hall_Button.png"))
+hall_image = Image.open("Level_5/Image/Hall_Button.png")
+hall_glow_image = create_glow_effect(hall_image)
+hall_button_image = ImageTk.PhotoImage(hall_glow_image)
 hall_button = canvas.create_image(655/1280*screen_width, 217/720*screen_height, image=hall_button_image)
 canvas.tag_bind(hall_button, "<Button-1>", open_hall_window)
 
@@ -35,7 +51,9 @@ def open_room_window(event):
     subprocess.Popen(["python", "Level_5/Room.py"])
     root.destroy()
 
-room_button_image = ImageTk.PhotoImage(Image.open("Level_5/Image/Room_Button.png"))
+room_image = Image.open("Level_5/Image/Room_Button.png")
+room_glow_image = create_glow_effect(room_image)
+room_button_image = ImageTk.PhotoImage(room_glow_image)
 room_button = canvas.create_image(211/1280*screen_width, 367/720*screen_height, image=room_button_image)
 canvas.tag_bind(room_button, "<Button-1>", open_room_window)
 
